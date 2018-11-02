bed1f4159467ca79de4f8b1d55d026081f3c2378
bill <williamandrewbain@gmail.com>
2018-11-02 11:46:12 -0600

    added routes for students and beers

--- controllers/studentControllers.js
+++ controllers/studentControllers.js
@@ -6,16 +6,41 @@ const getAll = (req, res, next) => {
         .then(students => res.json({students: students}))
 }
 const getOne = (req, res, next) => {
-    
+    let id = req.params.id
+    return knex('student')
+        .where('id', id)
+        .then(student => { res.json({ student: student[0]}) })
 }
 const postStudents = (req, res, next) => {
-    
+    let body = req.body
+    return knex('student')
+        .insert(body)
+        .returning('*')
+        .then(student => {
+            res.json({student: student[0]})
+        })
 }
 const putStudents = (req, res, next) => {
-    
+    let body = req.body
+    let id = req.params.id
+
+    return knex('student')
+        .where('id', id)
+        .update(body)
+        .returning('*')
+        .then(student => {
+            res.json({ student: student[0] })
+        })
 }
 const deleteStudents = (req, res, next) => {
-    
+    let id = req.params.id
+    return knex('student')
+        .where('id', id)
+        .delete()
+        .returning('*')
+        .then(student => {
+            res.json({ deleteStudent: student[0] })
+        })
 }
 
 module.exports = {
