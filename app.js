1d3d0689c1511229390e98ac26e4eb6ff34d1d06
Kyle Czajkowski <skiroyjenkins@gmail.com>
2018-11-02 11:30:59 -0600

    added error handling

--- app.js
+++ app.js
@@ -3,14 +3,39 @@ const app = express()
 const port = process.env.PORT || 3000
 const beerRoutes = require('./routes/beerRoutes')
 const studentRoutes = require('./routes/studentRoutes')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors())
 
 app.get('/', (req, res) => {
-  res.json('🍄')
+  process.env.NODE_ENV !== 'production' ?
+  res.json({
+    beers: `http://localhost:${port}/beers`,
+    students: `http://localhost:${port}/students`
+  }) :
+  res.json({
+    beers: `deployed link`,
+    students: `deployed link`
+  })
 })
 
 app.use('/beers', beerRoutes)
 app.use('/students', studentRoutes)
 
+app.use(notFound);
+app.use(errorHandler);
 
-app.listen(port, () => {console.log(`Up on port ${port}` )})
+function notFound(err, req, res, next) {
+    res.status(404).send({error: 'Not found!', status: 404, url: req.originalUrl})
+}
+
+function errorHandler(err, req, res, next) {
+    console.error('NOPE, LOL', err)
+    const stack =  process.env.NODE_ENV !== 'production' ? err.stack : undefined
+    res.status(500).send({error: err.message, stack, url: req.originalUrl})
+}
+
+app.listen(port, () => {console.log(`Up on port http://localhost:${port}` )})
